7d1eca3984eb2e36722d65809580a93a38ac95f9
Antonina S <antoninad22@gmail.com>
2025-05-02 00:53:26 -0500

    refactored SavedBooks.tsx

--- client/src/pages/SavedBooks.tsx
+++ client/src/pages/SavedBooks.tsx
@@ -1,23 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useEffect, JSXElementConstructor, Key, ReactElement, ReactNode, ReactPortal } from 'react';
 import { Container, Card, Button, Row, Col } from 'react-bootstrap';
 
 import Auth from '../utils/auth.js';
 import { removeBookId } from '../utils/localStorage';
-import type { User } from '../models/User.js';
 import { useMutation, useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries.js';
 import { REMOVE_BOOK } from '../utils/mutations.js';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState<User>({
-    username: '',
-    email: '',
-    password: '',
-    savedBooks: [],
-  });
-
-  const userDataLength = Object.keys(userData).length;
-
   const { loading, data, refetch } = useQuery(QUERY_ME);
   const [removeBook] = useMutation(REMOVE_BOOK);
 
@@ -30,12 +20,7 @@ const SavedBooks = () => {
           return false;
         }
 
-        const { data: freshData } = await refetch(); 
-        const user = freshData?.me;
-
-        if (user) {
-          setUserData(user);
-        }
+        await refetch(); 
       } catch (err) {
         console.error(err);
       }
@@ -66,7 +51,7 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
@@ -74,8 +59,8 @@ const SavedBooks = () => {
     <>
       <div className='text-light bg-dark p-5'>
         <Container>
-          {userData.username ? (
-            <h1>Viewing {userData.username}'s saved books!</h1>
+          {data.me.username ? (
+            <h1>Viewing {data.me.username}'s saved books!</h1>
           ) : (
             <h1>Viewing saved books!</h1>
           )}
@@ -83,14 +68,14 @@ const SavedBooks = () => {
       </div>
       <Container>
         <h2 className='pt-5'>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? 'book' : 'books'
+          {data.me.savedBooks.length
+            ? `Viewing ${data.me.savedBooks.length} saved ${
+              data.me.savedBooks.length === 1 ? 'book' : 'books'
               }:`
             : 'You have no saved books!'}
         </h2>
         <Row>
-          {userData.savedBooks.map((book) => {
+          {data.me.savedBooks.map((book: { bookId: Key | null | undefined; image: string | undefined; title: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; authors: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; description: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => {
             return (
               <Col md='4' key={book.bookId}>
                 <Card border='dark'>
@@ -107,7 +92,7 @@ const SavedBooks = () => {
                     <Card.Text>{book.description}</Card.Text>
                     <Button
                       className='btn-block btn-danger'
-                      onClick={() => handleDeleteBook(book.bookId)}
+                      onClick={() => handleDeleteBook(book.bookId as string)}
                     >
                       Delete this Book!
                     </Button>
